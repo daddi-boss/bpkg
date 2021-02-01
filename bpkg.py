221ae02617e779b68e986717a7ccb2c72427bcff
Arjun Daddikar <arjun.daddikar@gmail.com>
2021-02-01 08:42:10 +0530

    Added clean cache option

--- bpkg.py
+++ bpkg.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import shutil
 
 from bundler import bundle
 from app_starter import start_app
@@ -17,13 +18,14 @@ def main():
     if sys.argv[1] == '-h':
         print(
             '''
-            bpkg syntax - bpkg [-h|-v|start|bundle|extract] foldername
+            bpkg syntax - bpkg [-h|-v|start|bundle|extract|cleanup] foldername
 
             bpkg is a tool used to bundle applications.
 
-            start - This command is used to start the application.
-            bundle - This command is used to bundle an application hence the name.
-            extract - This command is used to extract the contents of an application from its bundle.
+              cleanup - Cleans up the bpkg cache
+              start - This command is used to start the application.
+              bundle - This command is used to bundle an application hence the name.
+              extract - This command is used to extract the contents of an application from its bundle.
             '''
         )
         exit()
@@ -33,12 +35,18 @@ def main():
     
     if sys.argv[1] == 'start':
         start_app(sys.argv[2])
+    elif sys.argv[1] == 'cleanup':
+        for root, dirs, files in os.walk(os.path.expanduser('~/.bpkg')):
+            for f in files:
+                os.unlink(os.path.join(root, f))
+            for d in dirs:
+                shutil.rmtree(os.path.join(root, d))
     elif sys.argv[1] == 'bundle':
         bundle(sys.argv[2])
     elif sys.argv[1] == 'extract':
         extract(sys.argv[2])
     else:
-        print('bundle, start or extract should be present as the second argument')
+        print('bundle, start, cleanup or extract should be present as the second argument')
         exit()
 
 if __name__ == '__main__':
