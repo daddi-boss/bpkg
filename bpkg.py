9080dd8d54354ed5bdcb0704319869424229cc52
Arjun Daddikar <arjun.daddikar@gmail.com>
2021-02-02 11:06:55 +0530

    More bug fixes

--- bpkg.py
+++ bpkg.py
@@ -9,7 +9,12 @@ from extractor import extract
 def main():
     if not os.path.exists(os.path.expanduser('~/.bpkg')):
         os.mkdir(os.path.expanduser('~/.bpkg'))
-    
+
+    if len(sys.argv) == 1:
+        print('Error: No arguments present.')
+        os.system('python bpkg.py -h')
+        exit()
+
     # Change this each time version changes
     if sys.argv[1] == '-v':
         print('bpkg application packaging utility version 1.0.0')
@@ -18,14 +23,14 @@ def main():
     if sys.argv[1] == '-h':
         print(
             '''
-            bpkg syntax - bpkg [-h|-v|start|bundle|extract|cleanup] foldername
+bpkg syntax - bpkg [-h|-v|start|bundle|extract|cleanup] foldername
 
-            bpkg is a tool used to bundle applications.
+bpkg is a tool used to bundle applications.
 
-              cleanup - Cleans up the bpkg cache
-              start - This command is used to start the application.
-              bundle - This command is used to bundle an application hence the name.
-              extract - This command is used to extract the contents of an application from its bundle.
+    cleanup - Cleans up the bpkg cache
+    start - This command is used to start the application.
+    bundle - This command is used to bundle an application hence the name.
+    extract - This command is used to extract the contents of an application from its bundle.
             '''
         )
         exit()
